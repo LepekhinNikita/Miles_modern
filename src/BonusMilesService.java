e1872d5416488816ec705d32946520113a38b3a8
Lepehin Nikita <Lepekhinnikita@gmail.com>
2023-01-21 21:45:48 +0400

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int miles;
+        miles = (price / 20);
+    return miles;
+    }
+
+}
